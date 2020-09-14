f64bf843def4427c1aa1ab632f1f39927765f706
mariomediavilla <mario.jose.mediavilla@hotmail.com>
2020-09-14 12:30:23 +0200

    correciones

--- martians.js
+++ martians.js
@@ -1,95 +1,108 @@
+//const main = require("./nombre.js")
+
 upperRightCoordinates = new Array(2)
 robotData = new Array(3)
 instructions = new String(100)
-var scent = [[1,1],[2,1],[3,2],[5,2]];
-var coordX, coordY, orientation;
-
-
-function scentCoordinate(x, y, array) {
+var scentRobots = new Array(50)
+var lost = false;
+function scentedCoordinates(x, y, array) {
     array.push(x);
     array.push(y);
-    return array;
 }
 
 
-function verifyScentCoordinates(coordX, coordY,array) {
-    for (var i = 0; i < array.length; i++) {
-        if (coords[i].x == coordX && coords[i].y == coordY)
-            return false;
-        return true;
+
+
+function verifyIfOutOfBounds(positionX, positionY, gridLimits) {
+    if(positionX > gridLimits[0] || positionY > gridLimits[1] || positionX < 0 || positionY < 0){
+        scentedCoordinates(positionX,positionY,scentRobots); //como rompo el bucle de abajoo, con lost = true?
+        robotData[0] = positionX;
+        robotData[1] = positionY;
+
+        lost = true;
     }
+
+    return positionX > gridLimits[0] || positionY > gridLimits[1] || positionX < 0 || positionY < 0;
 }
 
+instructions = 'FRRFLLFFRRFLL'.split('');
+gridLimits = [5,3];
+robotData = [3, 2, 'N']
+
+const directionsToAngle = {
+    N: 0, // ¿¿¿N o 'N'???
+    E: 90,
+    S: 180,
+    W: 270
+}
+const anglesToDirection = {
+    0: 'N',
+    90: 'E',
+    180: 'S',
+    270: 'W'
+}
+
+function computeRobot(robotData, instructions, gridLimits) {
+    let angleRobot = directionsToAngle[robotData[2]];
+    let scentArray = new Array();
+    //let lost = false;
 
 
-instructions = ['RFRFRFRFRF']
-gridLimits = [5,1];
-robotData = [coordX, coordY, orientation]
+    for (let i = 0; i < instructions.length; i++) {
+        console.log(instructions[i]);
+        console.log(robotData);
+        console.log(scentArray)
 
-function movement() {
-    var angleRobot;
-    var scentArray;
-    var lost = false;
-    if (robotData[2] == 'N') {
-        angleRobot = 0;
-
-    } else if (robotData[2] == 'E') {
-        angleRobot = 90;
-
-    } else if (robotData[2] == 'S') {
-        angleRobot = 180;
-
-    } else angleRobot = 270;
-
-    for (var i = 0; i < instructions.length; i++) {
-        if (instructions.charAt(i) == 'F') {
-            if (robotData[2] == 'N' && verifyScentCoordinates(robotData[0],robotData[1]+1, scentArray)) {
+        if (instructions[i] === 'F') {
+            if (robotData[2] === 'N' && !verifyIfOutOfBounds(robotData[0],robotData[1]+1, gridLimits)) {
                 robotData[1]++;
+                console.log("F111");
 
 
-            } else if (robotData[2] == 'S' && verifyScentCoordinates(robotData[0],robotData[1]-1, scentArray)) {
+                //console.log(`X:  ${robotData[0]}  Y:   ${robotData[1]+1} Orientation:  ${robotData[2]} Krrrzzzzt...OOH NOO!..Krzzzzzzzztzzzz...\\n LOST\\n`);
+
+            } else if (robotData[2] === 'S' && !verifyIfOutOfBounds(robotData[0],robotData[1]-1, gridLimits)) {
                 robotData[1]--;
+                console.log("F222");
 
 
-            } else if (robotData[2] == 'E' && verifyScentCoordinates(robotData[0]+1,robotData[1], scentArray)) {
+            } else if (robotData[2] === 'E' && !verifyIfOutOfBounds(robotData[0]+1,robotData[1], gridLimits)) {
                 robotData[0]++;
+                console.log("F333");
 
 
             } else {
+                if (!verifyIfOutOfBounds(robotData[0]-1,robotData[1], gridLimits)){
+                    robotData[0]--;
+                }
+                console.log("Fcalvo");
 
-                if (verifyScentCoordinates(robotData[0]-1,robotData[1], scentArray)) robotData[0]--;;
             }
 
-        } else if (instructions.charAt(i) == 'R') {
+        } else if (instructions[i] === 'R') {
             angleRobot = angleRobot + 90;
+
         } else angleRobot = angleRobot - 90;
 
-        if (angleRobot == 360 || angleRobot == -360) angleRobot = 0;
+        if (angleRobot === 360 || angleRobot === -360) angleRobot = 0;
 
-        angleRobot = Math.abs(angleRobot);
+        robotData[2] = anglesToDirection[Math.abs(angleRobot)]
 
-        if (angleRobot == 0) {
-            robotData[2] = 'N';
+        if (lost){
+            scentArray = scent(robotData[0], robotData[1], scentArray);
+            //console.log(`X:  ${robotData[0]}  Y:   ${robotData[1]} Orientation:  ${robotData[2]} Krrrzzzzt...OOH NOO!..Krzzzzzzzztzzzz...\\n LOST\\n`);
+            return robotData;
 
-        } else if (angleRobot == 90) {
-            robotData[2] = 'E';
-
-        } else if (angleRobot == 180) {
-            robotData[2] = 'S';
-
-        } else robotData[2] = 'W';
-
-        if (robotData[0] > gridLimits[0] || robotData[1] > gridLimits[1] || robotData[0] < 0 || robotData[1] < 0) {
-            scentArray = scentCoordinate(robotData[0], robotData[1], scentArray);
-            print( "X:" + robotData[0] + "Y: " + robotData[1] + "Orientation: " + robotData[2] + "Krrrzzzzt...OOH NOO!..Krzzzzzzzztzzzz...\n LOST\n");
-            lost = true;
-            break;
         }
+        console.log(`Lost = ${lost}`);
+        if (lost) return robotData;
     }
-    if (!lost) print( "X:" + robotData[0] + "Y: " + robotData[1]);
+    if (!lost) //console.log(`X:  ${robotData[0]}  Y:   ${robotData[1]}  Orientation:  ${robotData[2]}`);
+    return robotData;
 
 }
-
-
+const lastPosition =  computeRobot();
+console.log(lastPosition)
+if(lost) console.log('LOST')
 
 
